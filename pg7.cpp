26e11b0aec5e0ae1f66c2e559804750737a41d58
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-09-03 14:08:26 +0530

    Create pg7.cpp

---
+++ pg7.cpp
@@ -0,0 +1,29 @@
+// Linear search
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a[10],data,i;
+    int size;
+    cout<<"Enter the size : ";
+    cin>>size;
+    cout<<"\nEnter the elements : "<<endl;
+    for(i=0;i<size;i++){
+        cin>>a[i];
+    }
+
+    cout<<"\nEnter the element for search : "<<endl;
+    cin>>data;
+
+    for(i=0;i<size;i++){
+        if(a[i]==data){
+            cout<<"\n The data "<<data<<" present in array";
+            break;
+        }
+    }
+
+    if(i==size){
+        cout<<"\n The data not present"<<endl;
+    }
+    return 0;
+}
